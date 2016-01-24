f2d4b77f3f4f695a4cc14bd5a0be255682a687f0
Sean Collins <sean@cllns.com>
2016-01-23 17:30:57 -0700

    Support file name or string as argument

--- lib/semantic_linefeeds/cli.rb
+++ lib/semantic_linefeeds/cli.rb
@@ -3,7 +3,25 @@ require "semantic_linefeeds/converter"
 module SemanticLinefeeds
   class CLI
     def initialize(args)
-      puts Converter.run(args[0])
+      if File.exist?(args[0])
+        input = File.read(args[0])
+      else
+        input = string_from_args(args)
+      end
+
+      puts Converter.run(input)
+    end
+
+    private
+
+    # If the user passes the string in surrounded by quotes, return that string.
+    # Else, if they don't, we'll join all the args into a string.
+    def string_from_args(args)
+      if args.is_a?(Array)
+        args.join(" ")
+      else
+        args
+      end
     end
   end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,12 +3,32 @@ require "semantic_linefeeds/cli"
 
 module SemanticLinefeeds
   describe CLI do
-    it "takes string as STDIN and outputs to STDOUT" do
-      expect do
-        CLI.new(["Hello, there (sean)"])
-      end.to output(
-        "Hello,\nthere\n(sean)\n"
-      ).to_stdout
+    describe "string as argument" do
+      it "takes string as STDIN and outputs to STDOUT" do
+        expect do
+          CLI.new(["Hello, there (sean)"])
+        end.to output(
+          "Hello,\nthere\n(sean)\n"
+        ).to_stdout
+      end
+    end
+
+    describe "file name as argument" do
+      before do
+        File.write("tmp.txt", "Hello, world (yes it is predictable)")
+      end
+
+      it "takes file name as STDIN and outputs to STDOUT" do
+        expect do
+          CLI.new(["tmp.txt"])
+        end.to output(
+          "Hello,\nworld\n(yes it is predictable)\n"
+        ).to_stdout
+      end
+
+      after do
+        File.delete("tmp.txt")
+      end
     end
   end
 end
